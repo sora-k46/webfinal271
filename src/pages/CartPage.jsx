6f33a703b248877c947feb37ef5d6087cbbb7a6b
sora-k46 <sorrawit456789@gmail.com>
2022-05-30 21:11:57 +0700

    fix overlapes

--- src/pages/CartPage.jsx
+++ src/pages/CartPage.jsx
@@ -5,9 +5,10 @@ import { CardText, Button, Card, CardImg, CardTitle, CardBody } from 'reactstrap
 function CartPage() {
   const cart = useSelector((state) => state.cart)
   return (
-    <div style={{ height: '90vh' }} className='container'>
+    <div className='container'>
       <div className="row">
           <p>Your order right now : {cart.reduce((sum, item) => sum + item.quantity, 0)}</p>
+          <p>Total : {cart.reduce((sum, item) => sum + item.quantity, 0) * 45} Baht</p>
           {cart.length === 0 ? <p>Cart is Empty</p> : cart.map((dish) => {
             return (
               <Card key={dish.id} className='col-12 col-md-3 my-3' >
